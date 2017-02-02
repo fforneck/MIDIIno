fdb8dfcf5a133b1f15c94b36753a45f393f3f62e
fornecf <Filipe.Forneck@ADP.com>
2017-02-02 11:11:11 -0500

    * Moved client code to its own folder * Added server code to its own folder

---
+++ server/midiino.ino
@@ -0,0 +1,167 @@
+//Piezo defines
+#define NUM_PIEZOS 1
+
+// Thresholds
+#define LOW_THRESHOLD 30     //anything < TRIGGER_THRESHOLD is treated as 0
+#define MEDIUM_THRESHOLD 50
+#define HIGH_THRESHOLD 100
+
+//first analog slot of piezos
+#define START_SLOT 0     
+
+//MIDI baud rate
+#define SERIAL_RATE 38400
+
+//Program defines
+//ALL TIME MEASURED IN MILLISECONDS
+#define SIGNAL_BUFFER_SIZE 100
+#define PEAK_BUFFER_SIZE 30
+#define MAX_TIME_BETWEEN_PEAKS 20
+#define MIN_TIME_BETWEEN_NOTES 50
+
+//map that holds the respective threshold to each piezo
+unsigned short thresholdMap[NUM_PIEZOS];
+
+//Ring buffers to store analog signal and peaks
+short currentSignalIndex[NUM_PIEZOS];
+short currentPeakIndex[NUM_PIEZOS];
+unsigned short signalBuffer[NUM_PIEZOS][SIGNAL_BUFFER_SIZE];
+unsigned short peakBuffer[NUM_PIEZOS][PEAK_BUFFER_SIZE];
+
+boolean noteReady[NUM_PIEZOS];
+unsigned short noteReadyVelocity[NUM_PIEZOS];
+boolean isLastPeakZeroed[NUM_PIEZOS];
+
+unsigned long lastPeakTime[NUM_PIEZOS];
+unsigned long lastNoteTime[NUM_PIEZOS];
+
+void setup()
+{
+  Serial.begin(SERIAL_RATE);
+  
+  //initialize globals
+  for(short i=0; i<NUM_PIEZOS; ++i)
+  {
+    currentSignalIndex[i] = 0;
+    currentPeakIndex[i] = 0;
+    memset(signalBuffer[i],0,sizeof(signalBuffer[i]));
+    memset(peakBuffer[i],0,sizeof(peakBuffer[i]));
+    noteReady[i] = false;
+    noteReadyVelocity[i] = 0;
+    isLastPeakZeroed[i] = true;
+    lastPeakTime[i] = 0;
+    lastNoteTime[i] = 0;    
+  }
+  
+  thresholdMap[0] = LOW_THRESHOLD;
+  thresholdMap[1] = LOW_THRESHOLD;
+  thresholdMap[2] = HIGH_THRESHOLD;
+  thresholdMap[3] = HIGH_THRESHOLD;
+  thresholdMap[4] = HIGH_THRESHOLD;
+  thresholdMap[5] = MEDIUM_THRESHOLD;  
+}
+
+void loop()
+{
+  unsigned long currentTime = millis();
+  
+  for(unsigned short i=0; i<NUM_PIEZOS; ++i)
+  {
+    //get a new signal from analog read
+    unsigned short newSignal = analogRead(i);
+    signalBuffer[i][currentSignalIndex[i]] = newSignal;
+    
+    //if new signal is 0
+    if(newSignal < thresholdMap[i])
+    {
+      if(!isLastPeakZeroed[i] && (currentTime - lastPeakTime[i]) > MAX_TIME_BETWEEN_PEAKS)
+      {
+        recordNewPeak(i,0);
+      }
+      else
+      {
+        //get previous signal
+        short prevSignalIndex = currentSignalIndex[i]-1;
+        if(prevSignalIndex < 0) prevSignalIndex = SIGNAL_BUFFER_SIZE-1;        
+        unsigned short prevSignal = signalBuffer[i][prevSignalIndex];
+        
+        unsigned short newPeak = 0;
+        
+        //find the wave peak if previous signal was not 0 by going
+        //through previous signal values until another 0 is reached
+        while(prevSignal >= thresholdMap[i])
+        {
+          if(signalBuffer[i][prevSignalIndex] > newPeak)
+          {
+            newPeak = signalBuffer[i][prevSignalIndex];        
+          }
+          
+          //decrement previous signal index, and get previous signal
+          prevSignalIndex--;
+          if(prevSignalIndex < 0) prevSignalIndex = SIGNAL_BUFFER_SIZE-1;
+          prevSignal = signalBuffer[i][prevSignalIndex];
+        }
+        
+        if(newPeak > 0)
+        {
+          recordNewPeak(i, newPeak);
+        }
+      }
+  
+    }
+        
+    currentSignalIndex[i]++;
+    if(currentSignalIndex[i] == SIGNAL_BUFFER_SIZE) currentSignalIndex[i] = 0;
+  }
+}
+
+void recordNewPeak(unsigned short slot, short newPeak)
+{
+  isLastPeakZeroed[slot] = (newPeak == 0);
+  
+  unsigned long currentTime = millis();
+  lastPeakTime[slot] = currentTime;
+  
+  //new peak recorded (newPeak)
+  peakBuffer[slot][currentPeakIndex[slot]] = newPeak;
+  
+  //1 of 3 cases can happen:
+  // 1) note ready - if new peak >= previous peak
+  // 2) note fire - if new peak < previous peak and previous peak was a note ready
+  // 3) no note - if new peak < previous peak and previous peak was NOT note ready
+  
+  //get previous peak
+  short prevPeakIndex = currentPeakIndex[slot]-1;
+  if(prevPeakIndex < 0) prevPeakIndex = PEAK_BUFFER_SIZE-1;        
+  unsigned short prevPeak = peakBuffer[slot][prevPeakIndex];
+   
+  if(newPeak > prevPeak && (currentTime - lastNoteTime[slot])>MIN_TIME_BETWEEN_NOTES)
+  {
+    noteReady[slot] = true;
+    if(newPeak > noteReadyVelocity[slot])
+      noteReadyVelocity[slot] = newPeak;
+  }
+  else if(newPeak < prevPeak && noteReady[slot])
+  {
+    noteFire(slot, noteReadyVelocity[slot]);
+    noteReady[slot] = false;
+    noteReadyVelocity[slot] = 0;
+    lastNoteTime[slot] = currentTime;
+  }
+  
+  currentPeakIndex[slot]++;
+  if(currentPeakIndex[slot] == PEAK_BUFFER_SIZE) currentPeakIndex[slot] = 0;  
+}
+
+void noteFire(unsigned short slot, unsigned short velocity)
+{
+  byte buf[6];
+  buf[0] = slot / 256;
+  buf[1] = slot % 256;
+  buf[2] = velocity / 256;
+  buf[3] = velocity % 256;
+  buf[4] = thresholdMap[slot] / 256;
+  buf[5] = thresholdMap[slot] % 256;
+  Serial.write(buf, 6);
+}
+
