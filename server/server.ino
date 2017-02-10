ddb08bce5f7785f31ef2ee31154248048882e148
fforneck <fforneck@gmail.com>
2017-02-10 01:05:33 -0500

    New Arduino code.

--- server/server.ino
+++ server/server.ino
@@ -1,167 +1,126 @@
-//Piezo defines
-#define NUM_PIEZOS 1
+#define NUMPADS 1  // number of pads
 
-// Thresholds
-#define LOW_THRESHOLD 30     //anything < TRIGGER_THRESHOLD is treated as 0
-#define MEDIUM_THRESHOLD 50
-#define HIGH_THRESHOLD 100
+int val;
+String inputString = "";
 
-//first analog slot of piezos
-#define START_SLOT 0     
+// config
+unsigned int piezoThresholds[NUMPADS];
+unsigned int relevantSamples[NUMPADS];
+unsigned int irrelevantNoise[NUMPADS];
 
-//MIDI baud rate
-#define SERIAL_RATE 38400
+// data
+byte state[NUMPADS];
+unsigned int peak[NUMPADS];
+unsigned long lastNoteStart[NUMPADS];
+unsigned int sampleCount[NUMPADS];
 
-//Program defines
-//ALL TIME MEASURED IN MILLISECONDS
-#define SIGNAL_BUFFER_SIZE 100
-#define PEAK_BUFFER_SIZE 30
-#define MAX_TIME_BETWEEN_PEAKS 20
-#define MIN_TIME_BETWEEN_NOTES 50
-
-//map that holds the respective threshold to each piezo
-unsigned short thresholdMap[NUM_PIEZOS];
-
-//Ring buffers to store analog signal and peaks
-short currentSignalIndex[NUM_PIEZOS];
-short currentPeakIndex[NUM_PIEZOS];
-unsigned short signalBuffer[NUM_PIEZOS][SIGNAL_BUFFER_SIZE];
-unsigned short peakBuffer[NUM_PIEZOS][PEAK_BUFFER_SIZE];
-
-boolean noteReady[NUM_PIEZOS];
-unsigned short noteReadyVelocity[NUM_PIEZOS];
-boolean isLastPeakZeroed[NUM_PIEZOS];
-
-unsigned long lastPeakTime[NUM_PIEZOS];
-unsigned long lastNoteTime[NUM_PIEZOS];
-
-void setup()
-{
-  Serial.begin(SERIAL_RATE);
-  
-  //initialize globals
-  for(short i=0; i<NUM_PIEZOS; ++i)
-  {
-    currentSignalIndex[i] = 0;
-    currentPeakIndex[i] = 0;
-    memset(signalBuffer[i],0,sizeof(signalBuffer[i]));
-    memset(peakBuffer[i],0,sizeof(peakBuffer[i]));
-    noteReady[i] = false;
-    noteReadyVelocity[i] = 0;
-    isLastPeakZeroed[i] = true;
-    lastPeakTime[i] = 0;
-    lastNoteTime[i] = 0;    
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(57600);
+  inputString.reserve(100);
+  for(byte piezo = 0; piezo < NUMPADS; piezo++) {
+    state[piezo] = 0;
+    piezoThresholds[piezo] = 50;
+    relevantSamples[piezo] = 15;
+    irrelevantNoise[piezo] = 100;
   }
-  
-  thresholdMap[0] = LOW_THRESHOLD;
-  thresholdMap[1] = LOW_THRESHOLD;
-  thresholdMap[2] = HIGH_THRESHOLD;
-  thresholdMap[3] = HIGH_THRESHOLD;
-  thresholdMap[4] = HIGH_THRESHOLD;
-  thresholdMap[5] = MEDIUM_THRESHOLD;  
 }
 
-void loop()
-{
-  unsigned long currentTime = millis();
-  
-  for(unsigned short i=0; i<NUM_PIEZOS; ++i)
-  {
-    //get a new signal from analog read
-    unsigned short newSignal = analogRead(i);
-    signalBuffer[i][currentSignalIndex[i]] = newSignal;
-    
-    //if new signal is 0
-    if(newSignal < thresholdMap[i])
-    {
-      if(!isLastPeakZeroed[i] && (currentTime - lastPeakTime[i]) > MAX_TIME_BETWEEN_PEAKS)
-      {
-        recordNewPeak(i,0);
-      }
-      else
-      {
-        //get previous signal
-        short prevSignalIndex = currentSignalIndex[i]-1;
-        if(prevSignalIndex < 0) prevSignalIndex = SIGNAL_BUFFER_SIZE-1;        
-        unsigned short prevSignal = signalBuffer[i][prevSignalIndex];
-        
-        unsigned short newPeak = 0;
-        
-        //find the wave peak if previous signal was not 0 by going
-        //through previous signal values until another 0 is reached
-        while(prevSignal >= thresholdMap[i])
-        {
-          if(signalBuffer[i][prevSignalIndex] > newPeak)
-          {
-            newPeak = signalBuffer[i][prevSignalIndex];        
-          }
-          
-          //decrement previous signal index, and get previous signal
-          prevSignalIndex--;
-          if(prevSignalIndex < 0) prevSignalIndex = SIGNAL_BUFFER_SIZE-1;
-          prevSignal = signalBuffer[i][prevSignalIndex];
+void loop() {
+
+  for(byte piezo = 0; piezo < NUMPADS; piezo++) {
+    switch(state[piezo]) {
+      case 0:
+        val = analogRead(piezo);
+        if (val >= piezoThresholds[piezo]) {
+          state[piezo] = 1;
+          lastNoteStart[piezo] = millis();
+          sampleCount[piezo] = 1;
+          peak[piezo] = val;
         }
-        
-        if(newPeak > 0)
-        {
-          recordNewPeak(i, newPeak);
+        break;
+      case 1:
+        val = analogRead(piezo);
+        if (val > peak[piezo]) {
+          peak[piezo] = val;
         }
-      }
-  
+        sampleCount[piezo]++;
+        if (sampleCount[piezo] >= relevantSamples[piezo]) {
+          Serial.print(piezo);
+          Serial.print(";");
+          Serial.print(peak[piezo]);
+          Serial.print(";");
+          Serial.print(lastNoteStart[piezo]);
+          Serial.print(";");
+          Serial.print(piezoThresholds[piezo]);
+          Serial.print(";");
+          Serial.print(relevantSamples[piezo]);
+          Serial.print(";");
+          Serial.print(irrelevantNoise[piezo]);
+          Serial.println();
+          /*byte buf[11];
+          buf[ 0] = piezo;
+          buf[ 1] = lowByte(peak[piezo]));
+          buf[ 2] = highByte(peak[piezo]));
+          buf[ 3] = (byte) lastNoteStart[piezo];
+          buf[ 4] = (byte) lastNoteStart[piezo] >> 8;
+          buf[ 5] = (byte) lastNoteStart[piezo] >> 16;
+          buf[ 6] = (byte) lastNoteStart[piezo] >> 24;
+          buf[ 7] = lowByte(piezoThresholds[piezo]));
+          buf[ 8] = highByte(piezoThresholds[piezo]));
+          buf[ 9] = lowByte(relevantSamples[piezo]));
+          buf[10] = highByte(relevantSamples[piezo]));
+          buf[11] = lowByte(irrelevantNoise[piezo]));
+          buf[12] = highByte(irrelevantNoise[piezo]));
+          Serial.write(buf, 6);*/
+          state[piezo] = 2;
+        }
+        break;
+      case 2:
+        if (lastNoteStart[piezo] + irrelevantNoise[piezo] <= millis()) {
+          state[piezo] = 0;
+        }
+        break;
     }
-        
-    currentSignalIndex[i]++;
-    if(currentSignalIndex[i] == SIGNAL_BUFFER_SIZE) currentSignalIndex[i] = 0;
   }
 }
 
-void recordNewPeak(unsigned short slot, short newPeak)
-{
-  isLastPeakZeroed[slot] = (newPeak == 0);
-  
-  unsigned long currentTime = millis();
-  lastPeakTime[slot] = currentTime;
-  
-  //new peak recorded (newPeak)
-  peakBuffer[slot][currentPeakIndex[slot]] = newPeak;
-  
-  //1 of 3 cases can happen:
-  // 1) note ready - if new peak >= previous peak
-  // 2) note fire - if new peak < previous peak and previous peak was a note ready
-  // 3) no note - if new peak < previous peak and previous peak was NOT note ready
-  
-  //get previous peak
-  short prevPeakIndex = currentPeakIndex[slot]-1;
-  if(prevPeakIndex < 0) prevPeakIndex = PEAK_BUFFER_SIZE-1;        
-  unsigned short prevPeak = peakBuffer[slot][prevPeakIndex];
-   
-  if(newPeak > prevPeak && (currentTime - lastNoteTime[slot])>MIN_TIME_BETWEEN_NOTES)
-  {
-    noteReady[slot] = true;
-    if(newPeak > noteReadyVelocity[slot])
-      noteReadyVelocity[slot] = newPeak;
-  }
-  else if(newPeak < prevPeak && noteReady[slot])
-  {
-    noteFire(slot, noteReadyVelocity[slot]);
-    noteReady[slot] = false;
-    noteReadyVelocity[slot] = 0;
-    lastNoteTime[slot] = currentTime;
-  }
-  
-  currentPeakIndex[slot]++;
-  if(currentPeakIndex[slot] == PEAK_BUFFER_SIZE) currentPeakIndex[slot] = 0;  
-}
+byte inputBytes[7];
+byte bytesRead = 0;
+void serialEvent(){  
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      int div1 = inputString.indexOf(";");
+      int div2 = inputString.indexOf(";", div1 + 1);
+      int div3 = inputString.indexOf(";", div2 + 1);
+      int piezo = inputString.substring(0,div1).toInt();
+      piezoThresholds[piezo] = inputString.substring(div1 + 1, div2).toInt();
+      relevantSamples[piezo] = inputString.substring(div2 + 1, div3).toInt();
+      irrelevantNoise[piezo] = inputString.substring(div3 + 1).toInt();
+      Serial.print("Setting piezo ");
+      Serial.print(piezo);
+      Serial.print(" piezoThresholds ");
+      Serial.print(piezoThresholds[piezo]);
+      Serial.print(" relevantSamples ");
+      Serial.print(relevantSamples[piezo]);
+      Serial.print(" irrelevantNoise ");
+      Serial.println(irrelevantNoise[piezo]);
+      inputString = "";
+    }
 
-void noteFire(unsigned short slot, unsigned short velocity)
-{
-  byte buf[6];
-  buf[0] = slot / 256;
-  buf[1] = slot % 256;
-  buf[2] = velocity / 256;
-  buf[3] = velocity % 256;
-  buf[4] = thresholdMap[slot] / 256;
-  buf[5] = thresholdMap[slot] % 256;
-  Serial.write(buf, 6);
+    
+    /*inputBytes[bytesRead++] = Serial.read();
+    if (bytesRead == 7) {
+      piezoThresholds[inputBytes[0]] = (unsigned int) (inputBytes[1] << 8) | inputBytes[2];
+      relevantSamples[inputBytes[0]] = (unsigned int) (inputBytes[3] << 8) | inputBytes[4];
+      irrelevantNoise[inputBytes[0]] = (unsigned int) (inputBytes[5] << 8) | inputBytes[6];
+      bytesRead = 0;
+    }*/
+  }
 }
-
